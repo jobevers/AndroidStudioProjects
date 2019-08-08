485ec0117c699525cece6ea0107a3aa26f732a99
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2019-08-07 22:50:35 -0400

    more cluelessness

---
+++ SimpleBluetoothLeTerminal/app/src/main/java/de/kai_morich/simple_bluetooth_le_terminal/MyCallback.java
@@ -0,0 +1,101 @@
+package de.kai_morich.simple_bluetooth_le_terminal;
+
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCallback;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.bluetooth.BluetoothGattService;
+import android.util.Log;
+
+
+public class MyCallback extends BluetoothGattCallback {
+    private int idx;
+    private String TAG;
+
+    public MyCallback(int idx) {
+        super();
+        this.idx = idx;
+        TAG = "MyCallback: " + idx;
+    }
+
+    @Override
+    public void onPhyUpdate(BluetoothGatt gatt, int txPhy, int rxPhy, int status) {
+        super.onPhyUpdate(gatt, txPhy, rxPhy, status);
+        Log.i(TAG, "onPhyUpdate");
+    }
+
+    @Override
+    public void onPhyRead(BluetoothGatt gatt, int txPhy, int rxPhy, int status) {
+        super.onPhyRead(gatt, txPhy, rxPhy, status);
+        Log.i(TAG, "onPhyRead");
+    }
+
+    @Override
+    public void onConnectionStateChange(BluetoothGatt gatt, int status, int newState) {
+        super.onConnectionStateChange(gatt, status, newState);
+        Log.i(TAG, "onConnectionStateChange");
+        gatt.discoverServices();
+    }
+
+    @Override
+    public void onServicesDiscovered(BluetoothGatt gatt, int status) {
+        super.onServicesDiscovered(gatt, status);
+        Log.i(TAG, "onServicesDiscovered");
+        for (BluetoothGattService bs: gatt.getServices()) {
+            Log.i(TAG, "UUID: " + bs.getUuid());
+            for (BluetoothGattCharacteristic gc: bs.getCharacteristics()) {
+                for (BluetoothGattDescriptor gd: gc.getDescriptors()) {
+                    Log.i(TAG, "Descriptor: " + gd.getValue());
+                }
+            }
+        }
+    }
+
+    @Override
+    public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+        super.onCharacteristicRead(gatt, characteristic, status);
+        Log.i(TAG, "onCharacteristicRead");
+    }
+
+    @Override
+    public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
+        super.onCharacteristicWrite(gatt, characteristic, status);
+        Log.i(TAG, "onCharacteristicWrite");
+    }
+
+    @Override
+    public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+        super.onCharacteristicChanged(gatt, characteristic);
+        Log.i(TAG, "onCharacteristicChanged");
+    }
+
+    @Override
+    public void onDescriptorRead(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+        super.onDescriptorRead(gatt, descriptor, status);
+        Log.i(TAG, "onDescriptorRead");
+    }
+
+    @Override
+    public void onDescriptorWrite(BluetoothGatt gatt, BluetoothGattDescriptor descriptor, int status) {
+        super.onDescriptorWrite(gatt, descriptor, status);
+        Log.i(TAG, "onDescriptorWrite");
+    }
+
+    @Override
+    public void onReliableWriteCompleted(BluetoothGatt gatt, int status) {
+        super.onReliableWriteCompleted(gatt, status);
+        Log.i(TAG, "onReliableWriteCompleted");
+    }
+
+    @Override
+    public void onReadRemoteRssi(BluetoothGatt gatt, int rssi, int status) {
+        super.onReadRemoteRssi(gatt, rssi, status);
+        Log.i(TAG, "onReadRemoteRssi");
+    }
+
+    @Override
+    public void onMtuChanged(BluetoothGatt gatt, int mtu, int status) {
+        super.onMtuChanged(gatt, mtu, status);
+        Log.i(TAG, "onMtuChanged");
+    }
+}
---
+++ SimpleBluetoothLeTerminal/app/src/main/java/de/kai_morich/simple_bluetooth_le_terminal/PatternGenerator.java
@@ -0,0 +1,20 @@
+package de.kai_morich.simple_bluetooth_le_terminal;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+
+public class PatternGenerator extends IntentService {
+    /**
+     * A constructor is required, and must call the super <code><a href="/reference/android/app/IntentService.html#IntentService(java.lang.String)">IntentService(String)</a></code>
+     * constructor with a name for the worker thread.
+     */
+    public PatternGenerator() {
+        super("PatternGeneratorService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent workIntent) {
+        int i=0;
+    }
+}
