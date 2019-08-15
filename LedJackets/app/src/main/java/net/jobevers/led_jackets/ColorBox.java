c22aecc07ca0dd1ff6149d45201d53b3bb073f17
Job Evers-Meltzer <jobevers@users.noreply.github.com>
2019-08-14 20:00:38 -0400

    more stuff is working

--- LedJackets/app/src/main/java/net/jobevers/led_jackets/ColorBox.java
+++ LedJackets/app/src/main/java/net/jobevers/led_jackets/ColorBox.java
@@ -3,6 +3,9 @@ package net.jobevers.led_jackets;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 
 import android.graphics.Color;
@@ -28,6 +31,7 @@ public class ColorBox extends AppCompatActivity {
     private ArrayList<BluetoothDevice> devices = new ArrayList<>();
     private ArrayList<BluetoothGatt> bluetoothGatts = new ArrayList<>();
     private ArrayList<MyCallback> callbacks = new ArrayList<>();
+    private ArrayList<Boolean> connected = new ArrayList<>();
     private long nextKeyFrame;
 
     @Override
@@ -40,25 +44,60 @@ public class ColorBox extends AppCompatActivity {
             BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
             // Pass through the bundle and get all of the devices
             Bundle bundle = getIntent().getExtras();
-            int nDevices = bundle.getInt("nDevices");
+            nDevices = bundle.getInt("nDevices");
+            Log.i(TAG, "nDevices: " + nDevices);
             for (int i = 0; i < nDevices; i++) {
                 String address = bundle.getString("device" + i);
                 deviceAddresses.add(address);
                 BluetoothDevice device = bluetoothAdapter.getRemoteDevice(address);
                 devices.add(device);
-                MyCallback callback = new MyCallback(i);
+                MyCallback callback = new MyCallback(i, this);
                 callbacks.add(callback);
-                BluetoothGatt gatt = device.connectGatt(getApplicationContext(), false, callback);
-                bluetoothGatts.add(gatt);
-                Log.i(TAG, "GATT Connected: " + i);
+                connected.add(false);
             }
+            connectGatt(0);
         } else {
             // What do I do here?
         }
 
         mHandler = new Handler();
-        mColorSetter.run();
+    }
+
+    void connectGatt(int idx) {
+        BluetoothDevice device = devices.get(idx);
+        MyCallback callback = callbacks.get(idx);
+        BluetoothGatt gatt = device.connectGatt(getApplicationContext(), false, callback);
+        bluetoothGatts.add(gatt);
+        Log.i(TAG, "GATT Connecting: " + idx);
+    }
+
+    void onConnection(int idx) {
+        connected.set(idx, true);
+        // set to red on connection
+        callbacks.get(idx).sendColor(0x00);
+        for (int i = 0; i < nDevices; i++) {
+            if (!this.connected.get(i)) {
+                connectGatt(i);
+                return;
+            }
+        }
+        // if we get here, all the devices are connected
+        // so we can start running
         nextKeyFrame = System.currentTimeMillis();
+        Log.i(TAG, "EVERYTHING IS CONNECTED");
+        // Initializes Bluetooth adapter.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        for (BluetoothDevice dev : bluetoothManager.getConnectedDevices(BluetoothProfile.GATT)) {
+            Log.i(TAG, "Connected: " + dev.getAddress());
+        }
+        // Calling via handler because we need to make sure this runs in the UI thread
+        mHandler.postDelayed(mColorSetter, 0);
+    }
+
+    private boolean areAllConnected() {
+        for (boolean b : this.connected) if (!b) return false;
+        return true;
     }
 
     Runnable mColorSetter = new Runnable() {
--- LedJackets/app/src/main/java/net/jobevers/led_jackets/MyCallback.java
+++ LedJackets/app/src/main/java/net/jobevers/led_jackets/MyCallback.java
@@ -1,12 +1,15 @@
 package net.jobevers.led_jackets;
 
+import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
+import android.bluetooth.BluetoothManager;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
+import android.graphics.Color;
 import android.util.Log;
 
 import java.util.UUID;
@@ -14,8 +17,9 @@ import java.util.concurrent.TimeUnit;
 
 
 public class MyCallback extends BluetoothGattCallback {
-    private int idx;
     private String TAG;
+    private int idx;
+    private ColorBox parent;
 
     private static final UUID BLUETOOTH_LE_CC254X_SERVICE = UUID.fromString("0000ffe0-0000-1000-8000-00805f9b34fb");
     private static final UUID BLUETOOTH_LE_CC254X_CHAR_RW = UUID.fromString("0000ffe1-0000-1000-8000-00805f9b34fb");
@@ -24,11 +28,13 @@ public class MyCallback extends BluetoothGattCallback {
     private int hue = 0;
     private long time;
     private boolean wait = true;
+    private int nextMessage;
 
-    public MyCallback(int idx) {
+    public MyCallback(int idx, ColorBox parent) {
         super();
         this.idx = idx;
-        TAG = "MyCallback: " + idx;
+        this.parent = parent;
+        TAG = "MyCallback-" + idx;
     }
 
     @Override
@@ -64,35 +70,47 @@ public class MyCallback extends BluetoothGattCallback {
         BluetoothGattService service = gatt.getService(BLUETOOTH_LE_CC254X_SERVICE);
         characteristic = service.getCharacteristic(BLUETOOTH_LE_CC254X_CHAR_RW);
         wait = false;
+        parent.onConnection(this.idx);
     }
 
     public void sendColor(int hue) {
         if (characteristic == null) {
             Log.i(TAG, "Waiting for service discovery");
+            // Calling connect here is dumb, but I've had issues with
+            // disconnects so this might force it
+            if (gatt != null) {
+                Log.w(TAG, "Attempting to reconnect");
+                gatt.connect();
+            }
             return;
         }
         if (wait) {
             Log.i(TAG, "Still waiting for acknowledgement so skipping. Sad.");
             return;
         }
+        this.hue = hue;
+        sendMessage(0);
+    }
+
+    private void sendMessage(int msg) {
         // TODO: a queue would be better here so that
         // I know that I'm not overwhelming the writeCharacteristic
         // and should wait until onCharacteristicWrite has finished
         int sat = 0xFF;
         int val = 0xFF;
-        byte[] data = {
-                pack((byte)hue,        (byte)sat, (byte)val),
-                pack((byte)(hue + 10), (byte)sat, (byte)val),
-                pack((byte)(hue + 20), (byte)sat, (byte)val),
-                // The remaining 17 bytes are filler
-                (byte) 0xFF, (byte) 0xFF,
-                (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-                (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF,
-                (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF, (byte) 0xFF
-        };
+        byte[] data = new byte[20];
+        data[0] = (byte) msg;
+        // For some reason I convinced myself that interlaced is better
+        // But it the way the arduino is coded right now I wait
+        // for the entire frame to be recieved, so it doesn't really matter.
+        // Maybe some poin in the future I could be more clever....
+        for (int i = 0; (2 * i + msg) < 25; i++) {
+            data[i + 1] = pack((byte) (hue + 10 * (2 * i + msg)), (byte) sat, (byte) val);
+        }
         characteristic.setValue(data);
-        Log.i(TAG, "Writing color " + hue + " : " + (hue & 0xF0));
+        Log.i(TAG, "Writing msg: " + msg + ". color " + hue + " : " + (hue & 0xF0));
         wait = true;
+        nextMessage = (msg + 1) % 2;
         gatt.writeCharacteristic(characteristic);
     }
 
@@ -101,20 +119,32 @@ public class MyCallback extends BluetoothGattCallback {
         // The 2 most significant bits of the sat / val
         // The latter two need to be shifted 4 and 6 bits respectively
         // The end result is 8 bits: hhhhssvv.
-        return (byte)((hue & 0xF0) | ((sat & 0xC0) >> 4) | ((val & 0xC0) >> 6));
+        return (byte) ((hue & 0xF0) | ((sat & 0xC0) >> 4) | ((val & 0xC0) >> 6));
     }
 
     @Override
     public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
         super.onCharacteristicRead(gatt, characteristic, status);
-        wait = false;
-        Log.i(TAG, "onCharacteristicRead: " + characteristic.getValue());
+        byte[] val = characteristic.getValue();
+        int msg = (int) val[0];
+        Log.i(TAG, "onCharacteristicRead: " + val);
+        Log.i(TAG, "onCharacteristicRead: msg: " + msg);
+        // TODO: would be nice to get the actual message from the arduino
+        // but in the meantime, just assume things have been sent 0 and then 1.
+        if (nextMessage == 1) {
+            // TODO: can I be more aggressive here and send the second message
+            // right after the onCharacteristicWrite?
+            sendMessage(1);
+        } else {
+            wait = false;
+        }
     }
 
     @Override
     public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status) {
         super.onCharacteristicWrite(gatt, characteristic, status);
         Log.i(TAG, "onCharacteristicWrite");
+        Log.i(TAG, "Trigger characteristicRead");
         gatt.readCharacteristic(characteristic);
     }
 
